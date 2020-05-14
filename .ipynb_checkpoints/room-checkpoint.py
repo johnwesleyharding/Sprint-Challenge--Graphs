1585ff82fce9c8432ea00214602bce6432ed352e
johnwesleyharding <johnwesleyharding@users.noreply.github.com>
2020-05-14 16:20:52 -0600

    novelty 1010

--- .ipynb_checkpoints/room-checkpoint.py
+++ .ipynb_checkpoints/room-checkpoint.py
@@ -1,4 +1,5 @@
-
+# Implement a class to hold room information. This should have name and
+# description attributes.
 class Room:
     def __init__(self, name, description, id=0, x=None, y=None):
         self.id = id
